6629453a6a50b9669600b2ae15c0d99a68279f9c
AdrianaBannykh <37584523+AdrianaBannykh@users.noreply.github.com>
2018-10-01 15:34:38 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,97 +3,29 @@ using namespace std;
 
 /*
 Банных Адрианы
-Реализовать функцию возведения числа a в степень b:
-a. Без рекурсии.
-b. Рекурсивно.
-c. *Рекурсивно, используя свойство чётности степени.
+Реализовать функцию перевода чисел из десятичной системы в двоичную, используя
+рекурсию.
 */
-void Building(int basis, int degree)
+void Del(int number)
 {
-	double result;
-	if (basis == 0 and degree == 0)
+	int ostatok=0;
+	int i = 0;
+	int*mas = new int[10];
+	if (number > 0)
 	{
-		result = 0;
-		cout << result;
-	}
-	if (basis == 0 && degree >0)
-	{
-		result = 0;
-		cout << result;
-	}
-
-	if (degree == 0 && basis >0)
-	{
-		result = 1;
-		cout << result;
-	}
-	if (degree < 0 && basis>0)
-	{
-		degree = abs(degree);
-		result = 1 / pow(basis, degree);
-		cout << result;
-	}
-	if (basis < 0)
-	{
-		if (basis % 2 == 1)
-		{
-			result = -pow(basis, degree);
-			cout << result;
-		}
-		else
-		{
-			result = pow(basis, degree);
-			cout << result;
-		}
-	}
-	if (basis > 0 && degree > 0)
-	{
-		result = pow(basis, degree);
-		cout << result;
+		ostatok = number % 2;
+		number = number / 2;
+		mas[i] = ostatok;
+		Del(number);	
+		cout << mas[i];
 	}
 }
 
-double Recursion(int a, int b)
-{
-	double result;
-	if (b == 0 && a>0)
-	{
-		return 1;
-	}
-	if (a == 0)
-	{
-		return 0;
-	}
-	if (b < 0)
-	{
-		return (1/ (double)a)*Recursion((double)a, b+1);
-	}
-	if (a < 0)
-	{
-		if (b % 2 == 1)
-		{
-			return -abs(a) * Recursion(abs(a), b - 1);
-		}
-		else
-		{
-			return abs(a)*Recursion(abs(a),b-1);
-		}
-	}
-	if (a > 0 && b > 0)
-	{
-		return a * Recursion(a, b-1);
-	}
-}
 void main()
 {
-	int basis;
-	int degree;
-	cout << " Enter basis" << endl;
-	cin >> basis;
-	cout << "Enter degree" << endl;
-	cin >> degree;
-	Building(basis,degree);
-	cout << endl;
-	cout << Recursion(basis, degree);
+	int number;
+	cout << "Enter number: " << endl;
+	cin >> number;
+	Del(number);
 	system("pause");
 }
